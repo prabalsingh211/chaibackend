ca5a64abe502a499002f521da1cf6f5f260b7928
Prabal Singh <cool.prabalsingh@gmail.com>
2024-05-13 08:35:20 +0530

    github data added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 //import  express  from 'express'
 const app = express();
@@ -8,6 +8,45 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+const gitHubData = {
+  login: "prabalsingh211",
+  id: 169626201,
+  node_id: "U_kgDOChxKWQ",
+  avatar_url: "https://avatars.githubusercontent.com/u/169626201?v=4",
+  gravatar_id: "",
+  url: "https://api.github.com/users/prabalsingh211",
+  html_url: "https://github.com/prabalsingh211",
+  followers_url: "https://api.github.com/users/prabalsingh211/followers",
+  following_url:
+    "https://api.github.com/users/prabalsingh211/following{/other_user}",
+  gists_url: "https://api.github.com/users/prabalsingh211/gists{/gist_id}",
+  starred_url:
+    "https://api.github.com/users/prabalsingh211/starred{/owner}{/repo}",
+  subscriptions_url:
+    "https://api.github.com/users/prabalsingh211/subscriptions",
+  organizations_url: "https://api.github.com/users/prabalsingh211/orgs",
+  repos_url: "https://api.github.com/users/prabalsingh211/repos",
+  events_url: "https://api.github.com/users/prabalsingh211/events{/privacy}",
+  received_events_url:
+    "https://api.github.com/users/prabalsingh211/received_events",
+  type: "User",
+  site_admin: false,
+  name: null,
+  company: null,
+  blog: "",
+  location: null,
+  email: null,
+  hireable: null,
+  bio: null,
+  twitter_username: null,
+  public_repos: 1,
+  public_gists: 0,
+  followers: 0,
+  following: 0,
+  created_at: "2024-05-13T01:23:00Z",
+  updated_at: "2024-05-13T02:41:33Z",
+};
+
 app.get("/twitter", (req, res) => {
   res.send("thakur_prabalsingh");
 });
@@ -16,9 +55,13 @@ app.get("/login", (req, res) => {
   res.send("<h1>Please login</h1>");
 });
 
-app.get('/youtube',(req,res)=>{
-    res.send('<h2>chai aur code')
-})
+app.get("/github", (req, res) => {
+  res.json(gitHubData);
+});
+
+app.get("/youtube", (req, res) => {
+  res.send("<h2>chai aur code");
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`);
